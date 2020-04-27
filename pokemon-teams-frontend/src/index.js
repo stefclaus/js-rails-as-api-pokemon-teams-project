a4d015af354d9efd66dd13e0888032bea40acb5b
github username <github email address>
2020-04-26 22:38:49 -0400

    add event handling

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -15,8 +15,58 @@ function renderTrainers(json) {
     trainerCard.setAttribute('class', 'card')
 
     trainerCard.dataset.id = trainer.id
-    trainerCard.innerHTML = `<p>${trainer.name}</p>`
+    trainerCard.innerHTML = `<p>${trainer.name}</p>
+    <button  data-trainer-id="${trainer.id}"> Add Pokemon </button>
+    <ul>
+      ${trainer.pokemons.map( pokemon => {
+      return `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-trainer-id="${trainer.id}"> Release </button> </li>`
+    }).join('')}
+     </ul>
+     `
+    trainerCard.addEventListener('click', handleButton)
     main.appendChild(trainerCard)
   })
 
+}
+
+const createPokemon = (trainerId) => {
+  return fetch(POKEMONS_URL, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'trainer_id': trainerId
+    })
+  })
+  .then(res => res.json())
+}
+
+function handleButton(event) {
+  if(event.target.tagName === "BUTTON") {
+    switch(event.target.innerText){
+      case 'Add Pokemon':
+      createPokemon(parseInt(event.target.dataset.trainerId))
+      .then(pokemon => {
+        if(!pokemon.error) {
+          let trainerCard = document.querySelector(`div[data-id='${pokemon["trainer_id"]}']`)
+          let pokemonList = trainerCard.querySelector('ul')
+          pokemonList.innerHTML += `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
+        }
+      })
+      break;
+      case 'Release':
+        let pokemonId = parseInt(event.target.dataset.pokemonId)
+        event.target.parentNode.remove()
+        handleRelease(pokemonID)
+      break;
+    }
+  }
+}
+
+const handleRelease = (pokemonID) => {
+  return fetch(`${POKEMONS_URL}/${pokemonId}`, {
+    method: "DELETE",
+      })
+    .then(resp => resp.json())
 }
