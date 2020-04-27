92a619cf4700e1d997e32b099d484c723b328ee9
github username <github email address>
2020-04-26 21:00:43 -0400

    connect trainer names in frontend

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,9 @@
 class TrainersController < ApplicationController
+
+  def index
+    trainers = Trainer.all
+
+    render json: trainers, include: [:pokemons], except: [:updated_at, :created_at]
+  end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,22 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetch(TRAINERS_URL)
+  .then(resp => resp.json())
+  .then(json => renderTrainers(json));
+
+
+function renderTrainers(json) {
+  const main = document.querySelector('main')
+
+  json.forEach(trainer => {
+    let trainerCard = document.createElement('div')
+    trainerCard.setAttribute('class', 'card')
+
+    trainerCard.dataset.id = trainer.id
+    trainerCard.innerHTML = `<p>${trainer.name}</p>`
+    main.appendChild(trainerCard)
+  })
+
+}
